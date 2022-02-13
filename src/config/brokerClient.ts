e3473da1838fc049952cd2bde15a61a0775dcd6b
Kenneth Freitas <kennethmf@outlook.com>
2022-02-12 21:42:34 -0300

    added docker-compose support

--- src/config/brokerClient.ts
+++ src/config/brokerClient.ts
@@ -1,13 +1,18 @@
-import { Kafka } from 'kafkajs';
+import { Kafka, KafkaConfig } from 'kafkajs';
 import { BROKER_ENVS } from './envs/brokerEnvs';
 
-export const brokerClient = new Kafka({
+const brokerConfigs: KafkaConfig = {
   clientId: BROKER_ENVS.CLIENT_ID,
   brokers: BROKER_ENVS.BROKERS_URLS?.split(','),
-  ssl: true,
-  sasl: {
+};
+
+if (BROKER_ENVS.USER && BROKER_ENVS.PASSWORD) {
+  brokerConfigs.ssl = true;
+  brokerConfigs.sasl = {
     mechanism: 'scram-sha-256',
     username: BROKER_ENVS.USER,
     password: BROKER_ENVS.PASSWORD,
-  },
-});
+  };
+}
+
+export const brokerClient = new Kafka(brokerConfigs);
