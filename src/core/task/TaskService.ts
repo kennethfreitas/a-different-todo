ecd2042e341f2ab6c7d518a4e86996bdb3a99f5f
Kenneth Freitas <kennethmf@outlook.com>
2022-02-23 14:23:15 -0300

    use private keyword

--- src/core/task/TaskService.ts
+++ src/core/task/TaskService.ts
@@ -8,14 +8,14 @@ export class TaskService {
   constructor(private readonly repository: TaskRepository) {}
 
   async createTask(newTask: CreateTaskDto): Promise<string> {
-    if (!this.#isNewTaskValid(newTask)) throw new Error('A task must valid a description and a responsible.');
+    if (!this.isNewTaskValid(newTask)) throw new Error('A task must valid a description and a responsible.');
 
     const id = nanoid();
     await this.repository.save({ id, ...newTask });
     return id;
   }
 
-  #isNewTaskValid(newTask: CreateTaskDto): boolean {
+  private isNewTaskValid(newTask: CreateTaskDto): boolean {
     const { description, responsible } = newTask;
     const isValidDescription = !!description && description.length > 10;
     const isValidResponsible = !!responsible;
